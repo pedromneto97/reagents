e96b6cf392478f08d1e6ddf5a0fada57fabe4f09
Pedro Neto <pedromneto97@gmail.com>
2020-06-03 20:40:52 -0300

    Add: Inicializa o hive e os dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:reagentdetection/models/reagent.dart';
+import 'package:reagentdetection/screens/screens.dart';
+import 'package:reagentdetection/services/bootService.dart';
 
-import 'screens/screens.dart';
-
-void main() {
+void main() async {
   runApp(MyApp());
+  await Hive.initFlutter();
+  Hive.registerAdapter(ReagentAdapter());
+  initializeData();
 }
 
 class MyApp extends StatelessWidget {
