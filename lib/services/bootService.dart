cad347a19cc6635e33886818d8d7a9d0fcbc2209
Pedro Neto <pedromneto97@gmail.com>
2020-06-03 20:39:41 -0300

    Add: Service que inicializa os dados

---
+++ lib/services/bootService.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+import 'package:reagentdetection/models/reagent.dart';
+
+void initializeData() async {
+  final box = await Hive.openBox<Reagent>("onuBox");
+  if (box.isEmpty) {
+    final jsonData = await rootBundle
+        .loadString("lib/assets/onu_table.json")
+        .then((value) => jsonDecode(value));
+    final List<Reagent> reagentList = List<Reagent>();
+    for (var item in jsonData) {
+      final Reagent reagent = Reagent()
+        ..nameAndDescription = item["nameAndDescription"]
+        ..numberONU = item["numberONU"]
+        ..riskClass = item["riskClass"]
+        ..riskNumber = item["riskNumber"]
+        ..limit = item["limit"];
+      reagentList.add(reagent);
+    }
+    box.addAll(reagentList);
+  }
+}
