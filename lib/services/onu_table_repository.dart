5f59b01d86f6f70bf1471bd078ca3aa75cdbb388
Pedro Neto <pedromneto97@gmail.com>
2021-07-23 23:24:46 -0300

    feat: adiciona service do firebase

---
+++ lib/services/onu_table_repository.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../exceptions/reagent_not_found.dart';
+import '../models/reagent.dart';
+
+class OnuTableRepository {
+  const OnuTableRepository._();
+
+  static const OnuTableRepository _instance = OnuTableRepository._();
+
+  factory OnuTableRepository() {
+    return _instance;
+  }
+
+  Future<Reagent> find({
+    required String riskNumber,
+    required int onuNumber,
+  }) async {
+    print(onuNumber);
+    print(riskNumber);
+    final data = await FirebaseFirestore.instance
+        .collection('onu_table')
+        .where(
+          'numberONU',
+          isEqualTo: onuNumber,
+        )
+        .where(
+          'riskNumber',
+          isEqualTo: riskNumber,
+        )
+        .limit(1)
+        .get();
+
+    if (data.size == 0) {
+      throw const ReagentNotFound();
+    }
+    final reagent = data.docs.first;
+    return Reagent(
+      nameAndDescription: reagent['nameAndDescription'],
+      numberONU: reagent['numberONU'],
+      riskClass: reagent['riskClass'],
+      limit: reagent['limit'],
+      riskNumber: reagent['riskNumber'],
+    );
+  }
+}
