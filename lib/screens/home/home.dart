7bccceadd754d05bea47e44f10131f8f779d8b55
Pedro Neto <pedromneto97@gmail.com>
2021-07-31 13:13:40 -0300

    feat: change layout to new interface

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -6,33 +6,76 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Detecção de reagentes',
-          textAlign: TextAlign.center,
-        ),
-        centerTitle: true,
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Bem vindo ao aplicativo de deteção de reagentes'),
-            ElevatedButton(
-              onPressed: () => Navigator.pushNamed(
-                context,
-                '/detect',
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text(
+                      'Detecção de Reagentes',
+                      style: Theme.of(context).textTheme.headline2,
+                      textAlign: TextAlign.center,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: Text.rich(
+                        const TextSpan(
+                          text:
+                              'Identificação de reagentes através da leitura do ',
+                          children: [
+                            TextSpan(
+                              text: 'painel de segurança.',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        style: Theme.of(context).textTheme.bodyText1,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              child: Row(
-                children: const [
-                  Text('Começar'),
-                  Icon(Icons.arrow_forward),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton.icon(
+                    onPressed: () => Navigator.pushNamed(
+                      context,
+                      '/detect',
+                    ),
+                    label: const Text(
+                      'Detectar Reagente',
+                    ),
+                    icon: const Icon(
+                      Icons.camera_alt_outlined,
+                      size: 40,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: OutlinedButton.icon(
+                      onPressed: () {},
+                      label: const Text(
+                        'Sobre',
+                      ),
+                      icon: const Icon(
+                        Icons.info_outline_rounded,
+                        size: 40,
+                      ),
+                    ),
+                  ),
                 ],
-                mainAxisSize: MainAxisSize.min,
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
