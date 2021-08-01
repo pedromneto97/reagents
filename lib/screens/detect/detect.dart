9d5fc2ec687b176ae0ddeff2393925adfc697f9b
Pedro Neto <pedromneto97@gmail.com>
2021-07-31 21:15:45 -0300

    feat: update screen interface and optimize camera flow

--- lib/screens/detect/detect.dart
+++ lib/screens/detect/detect.dart
@@ -1,10 +1,9 @@
-import 'dart:async';
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
 
-import '../../utils/camera_utils.dart';
+import '../../services/detect_service.dart';
+import '../../utils/regex.dart';
+import '../../widgets/clear_text_field_button/clear_text_field_button.dart';
 
 class Detect extends StatefulWidget {
   const Detect({Key? key}) : super(key: key);
@@ -13,219 +12,221 @@ class Detect extends StatefulWidget {
   _DetectState createState() => _DetectState();
 }
 
-class _DetectState extends State<Detect> {
+class _DetectState extends State<Detect> with WidgetsBindingObserver {
   final _formKey = GlobalKey<FormState>();
-  final firstLineController = TextEditingController();
-  final secondLineController = TextEditingController();
-  final secondLineFocusNode = FocusNode();
-  final TextDetector textRecognizer = GoogleMlKit.vision.textDetector();
-  final numberOnuRegex = RegExp(
-    r'\d{2,4}',
-  );
-  final riskNumberRegex = RegExp(
-    r'(x\d{2,3})|\d{2,4}',
-    caseSensitive: false,
-  );
+  final riskNumberController = TextEditingController();
+  final unNumberController = TextEditingController();
+  final DetectService detectService = DetectService();
   CameraController? cameraController;
-  late CameraDescription cameraDescription;
-  bool found = false;
   bool isDetecting = false;
-
-  Future<void> detectImage(InputImage image) async {
-    final processedImage = await textRecognizer.processImage(image);
-    for (TextBlock block in processedImage.blocks) {
-      List<TextLine> lines = block.lines;
-      if (lines.length == 2) {
-        String? firstLine = riskNumberRegex.stringMatch(lines.first.text);
-        String? secondLine = numberOnuRegex.stringMatch(lines[1].text);
-        if (firstLine != null && secondLine != null) {
-          firstLineController.text = firstLine;
-          secondLineController.text = secondLine;
-          setState(() {
-            found = true;
-          });
-          return;
-        }
-      }
-    }
-  }
-
-  clear() {
-    found = false;
-    firstLineController.clear();
-    secondLineController.clear();
-    startDetecting();
-  }
 
   void startDetecting() {
     cameraController!.startImageStream((CameraImage image) {
-      if (isDetecting || found) {
+      if (isDetecting) {
         return;
       }
       isDetecting = true;
-      detectImage(InputImage.fromBytes(
-        bytes: ScannerUtils.concatenatePlanes(image.planes),
-        inputImageData: ScannerUtils.buildMetaData(
-          image,
-          ScannerUtils.rotationIntToImageRotation(
-              cameraDescription.sensorOrientation),
-        ),
-      )).whenComplete(() => isDetecting = false);
+      detectService
+          .detect(
+        cameraImage: image,
+        sensorOrientation: cameraController!.description.sensorOrientation,
+      )
+          .then((value) {
+        if (value != null) {
+          riskNumberController.text = value.riskNumber ?? '';
+          unNumberController.text = value.unNumber.toString();
+        }
+      }).whenComplete(() => isDetecting = false);
     });
+  }
+
+  void listenUNNumber() {
+    if (unNumberController.text.isEmpty) {
+      startDetecting();
+      return;
+    }
+
+    if (cameraController!.value.isStreamingImages) {
+      cameraController!.stopImageStream();
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    ScannerUtils.getCamera(CameraLensDirection.back).then((value) {
-      cameraController = CameraController(
-        value,
-        ResolutionPreset.ultraHigh,
-        enableAudio: false,
-      );
-      cameraDescription = value;
-      cameraController!.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() {});
-        startDetecting();
-      });
+    availableCameras()
+        .then(
+          (cameras) => cameras.firstWhere(
+            (CameraDescription camera) =>
+                camera.lensDirection == CameraLensDirection.back,
+          ),
+        )
+        .then(onNewCameraSelected);
+    unNumberController.addListener(listenUNNumber);
+  }
+
+  void onNewCameraSelected(CameraDescription description) {
+    cameraController = CameraController(
+      description,
+      ResolutionPreset.ultraHigh,
+      enableAudio: false,
+    );
+
+    cameraController!.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+      startDetecting();
     });
   }
 
   @override
   void dispose() {
     cameraController?.dispose();
-    secondLineFocusNode.dispose();
-    firstLineController.dispose();
-    secondLineController.dispose();
-    textRecognizer.close();
+    riskNumberController.dispose();
+    unNumberController.removeListener(listenUNNumber);
+    unNumberController.dispose();
+    detectService.close();
     super.dispose();
   }
 
-  onSubmitFirstLine(v) => secondLineFocusNode.requestFocus();
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // App state changed before we got the chance to initialize.
+    if (cameraController == null || !cameraController!.value.isInitialized) {
+      return;
+    }
+    if (state == AppLifecycleState.inactive) {
+      if (cameraController != null) {
+        if (cameraController!.value.isStreamingImages) {
+          cameraController!.stopImageStream();
+        }
+        cameraController!.dispose();
+      }
+    } else if (state == AppLifecycleState.resumed) {
+      if (cameraController != null) {
+        onNewCameraSelected(cameraController!.description);
+      }
+    }
+  }
 
-  String? validateFirstLine(String? value) => !riskNumberRegex.hasMatch(value!)
+  String? validateRiskNumber(String? value) => !riskNumberRegex.hasMatch(value!)
       ? 'Não é um número de risco válido'
       : null;
 
-  String? validateSecondLine(String? value) =>
-      !numberOnuRegex.hasMatch(value!) ? 'Não é um número da ONU válido' : null;
+  String? validateONUNumber(String? value) =>
+      !unNumberRegex.hasMatch(value!) ? 'Não é um número da ONU válido' : null;
 
   navigate() {
     if (_formKey.currentState!.validate()) {
-      return Navigator.pushNamed(context, '/reagent', arguments: {
-        'riskNumber': firstLineController.text,
-        'numberOnu': int.parse(secondLineController.text),
-      });
+      Navigator.pushNamed(
+        context,
+        '/reagent',
+        arguments: {
+          'riskNumber': riskNumberController.text,
+          'numberOnu': int.parse(unNumberController.text),
+        },
+      );
     }
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: const Text(
-          'Existem campos inválidos',
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600),
-        ),
-        backgroundColor: Colors.red[900],
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detectar'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ConstrainedBox(
-              child: cameraController?.buildPreview() ??
-                  const CircularProgressIndicator(),
-              constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height / 2.5,
-                minHeight: 50.0,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.55,
+                child: Center(
+                  child: cameraController?.buildPreview() ??
+                      const CircularProgressIndicator(),
+                ),
               ),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextFormField(
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.text,
-                      textCapitalization: TextCapitalization.characters,
-                      decoration: InputDecoration(
-                        labelText: 'Primeira linha',
-                        hintText: 'Primeira linha da placa',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 32,
+                  right: 32,
+                  top: 24,
+                  bottom: 32,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        height: 66,
+                        child: TextFormField(
+                          textInputAction: TextInputAction.next,
+                          keyboardType: TextInputType.text,
+                          textCapitalization: TextCapitalization.characters,
+                          decoration: InputDecoration(
+                            labelText: 'Número de Risco',
+                            hintText: '1ª linha',
+                            suffixIcon: ClearTextFieldButton(
+                              controller: riskNumberController,
+                            ),
+                          ),
+                          controller: riskNumberController,
+                          validator: validateRiskNumber,
+                          maxLength: 4,
                         ),
                       ),
-                      controller: firstLineController,
-                      onFieldSubmitted: onSubmitFirstLine,
-                      validator: validateFirstLine,
-                      maxLength: 4,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: TextFormField(
-                      focusNode: secondLineFocusNode,
-                      textInputAction: TextInputAction.done,
-                      keyboardType: TextInputType.number,
-                      textCapitalization: TextCapitalization.characters,
-                      decoration: InputDecoration(
-                        labelText: 'Segunda linha',
-                        hintText: 'Segunda linha da placa',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                      Container(
+                        height: 66,
+                        margin: const EdgeInsets.only(
+                          top: 4.0,
+                          bottom: 24,
+                        ),
+                        child: TextFormField(
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.number,
+                          textCapitalization: TextCapitalization.characters,
+                          decoration: InputDecoration(
+                            labelText: 'Número da ONU',
+                            hintText: '2ª linha',
+                            suffixIcon: ClearTextFieldButton(
+                              controller: unNumberController,
+                            ),
+                          ),
+                          maxLength: 4,
+                          controller: unNumberController,
+                          validator: validateONUNumber,
+                          onFieldSubmitted: (_) => navigate(),
                         ),
                       ),
-                      maxLength: 4,
-                      controller: secondLineController,
-                      validator: validateSecondLine,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                if (found)
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: OutlinedButton(
-                        onPressed: clear,
-                        child: const Text('Limpar'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 12.0),
+                              child: OutlinedButton(
+                                onPressed: Navigator.of(context).pop,
+                                child: const Text('Voltar'),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: ElevatedButton(
+                                onPressed: navigate,
+                                child: const Text('Procurar'),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: ElevatedButton(
-                      onPressed: navigate,
-                      child: const Text('Procurar reagente'),
-                    ),
+                    ],
                   ),
                 ),
-              ],
-            )
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
